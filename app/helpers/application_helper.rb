0d029a74b2fa35a0e49176ff81ac37c4d78e1751
AhernShaia <gemgt4gtii@gmail.com>
2023-03-23 12:47:38 +0800

    調整CSS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+
+    def hello
+        [12,3,4,5,6,6].sample
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # resources :resumes
+  get "/resumes", to: "resumes#index"
+  get "/resumes/new", to: "resumes#new"
+
+  root "pages#home"
+  # get "/", to: "pages#home", as: "root"
+  get "/about", to: "pages#about"
+  get "/contact", to: "pages#contact"
 end
