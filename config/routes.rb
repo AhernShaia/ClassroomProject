bb1fae4653c0c05707aaea410aaf6896d1bbd793
AhernShaia <gemgt4gtii@gmail.com>
2023-03-30 11:26:41 +0800

    user_sign_up:routes desgin

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # resources :resumes
-  
+  resource :user, except: %i[new destroy] do
+    get "sign_up", action: "new"
+  end
 
-  get "/resumes/:id/edit", to: "resumes#edit", as: 'edit_resumes'
+  get "/resumes/:id/edit", to: "resumes#edit", as: "edit_resumes"
   get "/resumes", to: "resumes#index"
   get "/resumes/new", to: "resumes#new"
-  get "/resumes/:id", to: "resumes#show", as: 'resume'
+  get "/resumes/:id", to: "resumes#show", as: "resume"
   delete "/resumes/:id", to: "resumes#destroy"
   patch "/resumes/:id", to: "resumes#update"
   post "/resumes", to: "resumes#create"
   root "pages#home"
 
-  get "/resumes/@:userid/:slug", to: "resumes#slug"
-  
+  # get "/resumes/@:userid/:slug", to: "resumes#slug"
 
   get "/about", to: "pages#about"
   get "/contact", to: "pages#contact"
